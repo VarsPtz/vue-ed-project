5cfea04ddd5a3505fffc57b82be77c914956b055
Alexandr Varygin <otz-job@mail.ru>
2020-03-04 15:37:31 +0300

    life cycle of the directive

--- src/color.js
+++ src/color.js
@@ -1,5 +1,24 @@
 export default {
     bind(el, bindings, vnode) {
-        el.style.color = 'red'
+        console.log('bind')
+        el.style.color = 'blue'
+    },
+
+    inserted(el, bindings, vnode) {
+        console.log('inserted')
+    },
+
+    // oldVnode - старая виртуальная virtual node, которая была создана до апдейта
+    update(el, bindings, vnode, oldVnode) {
+        console.log('update')
+    },
+
+    componentUpdated(el, bindings, vnode, oldVnode) {
+        console.log('componentUpdated')
+    },
+
+    unbind() {
+        console.log('unbind')
     }
+
 }
