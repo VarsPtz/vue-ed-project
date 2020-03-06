1193acbd2ad82de3a56c757a41c25abfa77e3496
Alexandr Varygin <otz-job@mail.ru>
2020-03-06 17:07:44 +0300

    use modifiers

--- src/color.js
+++ src/color.js
@@ -1,8 +1,26 @@
 export default {
     bind(el, bindings, vnode) {
-        // el.style.color = 'blue'
-        const arg = bindings.arg
-        //el.style.color = bindings.value
-        el.style[arg] = bindings.value
+        //const arg = bindings.arg
+        //el.style[arg] = bindings.value
+
+        //const fontModifier = bindings.modifiers.font - рекомендуется использовать способ ниже
+        const fontModifier = bindings.modifiers['font']
+
+        if (fontModifier) {
+            el.style.fontSize = '30px'
+        }
+
+        const delayModifier = bindings.modifiers['delay']
+
+        let delay = 0
+
+        if (delayModifier) {
+            delay = 2000
+        }
+
+        setTimeout(() => {
+            const arg = bindings.arg
+            el.style[arg] = bindings.value
+        }, delay)
     }
 }
