05888b8314605c2cf3da4b4371469f039fab02e8
Alexandr Varygin <otz-job@mail.ru>
2020-03-13 16:36:19 +0300

    checkbox radio selec v-model.number

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,100 @@
 <template>
   <div>
-    <h2>{{ title }}</h2>
+    <h2>Form inputs</h2>
+    <input type="text" v-model.lazy="name">
+    <p>{{ name }}</p>
+    <hr>
+<!--    <textarea>{{ textarea }}</textarea>-->
+    <textarea v-model="textarea"></textarea>
+    <p>{{ textarea }}</p>
+    <hr>
+    <label>
+      <input type="checkbox" value="instagram" v-model="social"> Instagram
+    </label>
 
-    <input type="text" v-model="searchName">
+    <label>
+      <input type="checkbox" value="vk" v-model="social"> Vk
+    </label>
+
+    <label>
+      <input type="checkbox" value="facebook" v-model="social"> Facebook
+    </label>
 
     <ul>
-<!--      <li v-for="name of names">{{ name }}</li>-->
-      <li v-for="name of filteredNames">{{ name }}</li>
+      <li v-for="s in social">{{ s }}</li>
     </ul>
+    <hr>
+
+    <label>
+      <input type="radio" value="instagram" v-model="socialRadio"> Instagram
+    </label>
+
+    <label>
+      <input type="radio" value="vk" v-model="socialRadio"> Vk
+    </label>
+
+    <label>
+      <input type="radio" value="facebook" v-model="socialRadio"> Facebook
+    </label>
+
+    <p>{{ socialRadio }}</p>
+    <hr>
+
+<!--    <select>-->
+<!--      <option-->
+<!--        v-for="s in socialsList"-->
+<!--        :selected="s == defaultSocial"-->
+<!--      >{{ s }}</option>-->
+<!--    </select>-->
+
+    <select v-model="defaultSocialSelect">
+      <option
+        v-for="sel in socialsList"
+      >{{ sel }}</option>
+    </select>
+
+    <p>{{ defaultSocialSelect }}</p>
 
     <hr>
 
-    <app-list></app-list>
+    <input type="text" v-model.number="age">
+
+    <p>{{ age }}</p>
+
   </div>
 </template>
 
 <script>
 
-import ListMixin from "./listMixin"
-
 export default {
 
   data() {
       return {
-        title: 'Hello I am Vue'
+        name: 'Initial state',
+        textarea: 'I am initial text',
+        social: ['vk'],
+        socialRadio: 'facebook',
+        defaultSocial: 'facebook',
+        defaultSocialSelect: 'instagram',
+        socialsList: ['instagram', 'vk', 'facebook'],
+        age: 20
       }
   },
-  mixins: [ListMixin],
-  filters: {
-    lowercase(value) {
-      return value.toLowerCase()
+  watch: {
+    age (value) {
+      console.log(value)
     }
   }
 }
 </script>
 
 <style scoped>
+  textarea {
+    width: 400px;
+    height: 200px;
+  }
 
+  p {
+    white-space: pre;
+  }
 </style>
