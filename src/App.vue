64a809b5a9b170b281afd20193a9d9488d6509b2
Alexandr Varygin <otz-job@mail.ru>
2020-03-15 19:03:53 +0300

    Creating your own control VUE JS

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,32 @@
 <template>
   <div>
     <h2>Form inputs</h2>
-    <input type="text" v-model.lazy="name">
-    <p>{{ name }}</p>
-    <hr>
-<!--    <textarea>{{ textarea }}</textarea>-->
-    <textarea v-model="textarea"></textarea>
-    <p>{{ textarea }}</p>
-    <hr>
-    <label>
-      <input type="checkbox" value="instagram" v-model="social"> Instagram
-    </label>
 
-    <label>
-      <input type="checkbox" value="vk" v-model="social"> Vk
-    </label>
+    <app-onoff v-model="switchButton"></app-onoff>
 
-    <label>
-      <input type="checkbox" value="facebook" v-model="social"> Facebook
-    </label>
-
-    <ul>
-      <li v-for="s in social">{{ s }}</li>
-    </ul>
-    <hr>
-
-    <label>
-      <input type="radio" value="instagram" v-model="socialRadio"> Instagram
-    </label>
-
-    <label>
-      <input type="radio" value="vk" v-model="socialRadio"> Vk
-    </label>
-
-    <label>
-      <input type="radio" value="facebook" v-model="socialRadio"> Facebook
-    </label>
-
-    <p>{{ socialRadio }}</p>
-    <hr>
-
-<!--    <select>-->
-<!--      <option-->
-<!--        v-for="s in socialsList"-->
-<!--        :selected="s == defaultSocial"-->
-<!--      >{{ s }}</option>-->
-<!--    </select>-->
-
-    <select v-model="defaultSocialSelect">
-      <option
-        v-for="sel in socialsList"
-      >{{ sel }}</option>
-    </select>
-
-    <p>{{ defaultSocialSelect }}</p>
-
-    <hr>
-
-    <input type="text" v-model.number="age">
-
-    <p>{{ age }}</p>
-
+    <div>
+      <h3 v-if="switchButton">Component is enabled</h3>
+      <h3 v-else="switchButton">Component is disabled</h3>
+    </div>
   </div>
 </template>
 
 <script>
 
-export default {
+import Onoff from './Onoff.vue'
 
-  data() {
+  export default {
+
+    data() {
       return {
-        name: 'Initial state',
-        textarea: 'I am initial text',
-        social: ['vk'],
-        socialRadio: 'facebook',
-        defaultSocial: 'facebook',
-        defaultSocialSelect: 'instagram',
-        socialsList: ['instagram', 'vk', 'facebook'],
-        age: 20
+        switchButton: false
       }
   },
-  watch: {
-    age (value) {
-      console.log(value)
-    }
+  components: {
+    appOnoff: Onoff
   }
 }
+
 </script>
 
 <style scoped>
