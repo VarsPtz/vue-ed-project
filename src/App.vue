376260d449aad46bddcf84c7b385d7405827d032
Alexandr Varygin <otz-job@mail.ru>
2020-03-19 22:11:57 +0300

    Navigation creation with vue router (+ use bootstap nav)

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,19 @@
 <template>
-<!--  <div class="container" @submit.prevent="onSubmit">-->
-<!--    <form class="pt-3">-->
-<!--      <div class="form-group">-->
-<!--        <label for="email">Email</label>-->
-<!--        <input-->
-<!--          type="email"-->
-<!--          id="email"-->
-<!--          class="form-control"-->
-<!--          :class="{'is-invalid': $v.email.$error, 'is-valid': !$v.email.$error && $v.email.$dirty}"-->
-<!--          @blur="$v.email.$touch()"-->
-<!--          v-model="email"-->
-<!--        >-->
-<!--        <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>-->
-<!--        <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>-->
-<!--        <div class="invalid-feedback" v-if="!$v.email.uniqEmail">This email is already exists</div>-->
-<!--      </div>-->
-
-<!--      <div class="form-group">-->
-<!--        <label for="password">Password</label>-->
-<!--        <input-->
-<!--          type="password"-->
-<!--          id="password"-->
-<!--          class="form-control"-->
-<!--          :class="{'is-invalid': $v.password.$error}"-->
-<!--          @blur="$v.password.$touch()"-->
-<!--          v-model="password"-->
-<!--        >-->
-<!--        <div class="invalid-feedback" v-if="!$v.password.minLength">-->
-<!--          Min length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}-->
-<!--        </div>-->
-
-<!--      </div>-->
-
-<!--      <div class="form-group">-->
-<!--        <label for="confirm">Confirm password</label>-->
-<!--        <input-->
-<!--          type="password"-->
-<!--          id="confirm"-->
-<!--          class="form-control"-->
-<!--          :class="{'is-invalid': $v.confirmPassword.$error}"-->
-<!--          @blur="$v.confirmPassword.$touch()"-->
-<!--          v-model="confirmPassword"-->
-<!--        >-->
-<!--        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">-->
-<!--          Password should match-->
-<!--        </div>-->
-
-<!--      </div>-->
-
-<!--      <button-->
-<!--        class="btn btn-success"-->
-<!--        type="submit"-->
-<!--        :disabled="$v.$invalid"-->
-<!--      >Submit</button>-->
-<!--    </form>-->
-<!--  </div>-->
-
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link></router-link>
+<!--            <a class="nav-link" href="/">Home</a>-->
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="'/cars'">Cars</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
