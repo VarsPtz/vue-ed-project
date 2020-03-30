fb83061fc44c04a49797133a419b37ac0486209e
Alexandr Varygin <otz-job@mail.ru>
2020-03-30 11:26:35 +0300

    JS VUE Nested Roots

--- src/pages/Car.vue
+++ src/pages/Car.vue
@@ -2,7 +2,17 @@
   <div>
     <h1>Car id {{ id }}</h1>
 
-    <button class="btn btn-sm btn-info" @click="goBackToCars">Back</button>
+    <button class="btn btn-sm btn-default mb-2" style="background: grey;" @click="goBackToCars">Back</button>
+    <br>
+    <router-link
+      class="btn btn-info mb-2"
+      tag="button"
+      :to="'/car/' + id + '/full'"
+    >
+      Full info
+    </router-link>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
