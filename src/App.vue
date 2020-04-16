23300dc3f7f1ab3f7f3cdccb68c9039ca3acb87c
Alexandr Varygin <otz-job@mail.ru>
2020-04-16 10:42:42 +0300

    Retrieving objects from db.json database using GET request method

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,19 @@
     </div>
 
     <button class="btn btn-success" @click="createCar">Create car</button>
+    <button class="btn btn-primary" @click="loadCars">Load cars</button>
+
+    <hr>
+
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="car of cars"
+        :key="car.id"
+      >
+        <strong>{{ car.name }}</strong> - {{ car.year}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,7 +32,8 @@
     data () {
       return {
         carName: 'Ford',
-        carYear: 2018
+        carYear: 2018,
+        cars: []
       }
     },
     methods: {
@@ -38,6 +52,16 @@
          .then(newCar => {
            console.log(newCar)
          })
+      },
+      loadCars () {
+        this.$http.get('http://localhost:3000/cars')
+          .then(response => {
+            return response.json()
+          })
+          .then(cars => {
+            // console.log(cars)
+            this.cars = cars
+          })
       }
     }
   }
