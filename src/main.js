862523f522f7cc31ceb9413047b985c90e0b47ce
Alexandr Varygin <otz-job@mail.ru>
2020-04-21 23:29:33 +0300

    install and use vuex

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import store from './store'
 import request from "vue-resource/src/http/request";
-
-// регистрация плагина
-Vue.use(VueResource)
-
-// http без $ - т.к. это объект конфигурации
-Vue.http.options.root = 'http://localhost:3000/'
-
-// Intercepters
-Vue.http.interceptors.push(request => {
-  request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
-})
 
 new Vue({
   el: '#app',
+  // store: store,
+  store,
   render: h => h(App)
 })
